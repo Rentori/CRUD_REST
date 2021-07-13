1868ecf12c00f379744aa4b230b0732338c552f4
rentorry <vitya.erofeev.90@gmail.com>
2021-07-13 16:08:50 +0200

    changes

--- target/CRUD_REST-1.0-SNAPSHOT/WEB-INF/classes/db/migration/v4__create_user_events_schema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists user_events(
-                            user_id bigint not null,
-                            event_id bigint not null,
-                            foreign key (user_id) references users(id),
-                            foreign key (event_id) references events(id)
-)
