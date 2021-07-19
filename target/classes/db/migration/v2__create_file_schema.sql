505b00e5c96b83e482beacc668f108b1df6f907a
Rentori <50805135+Rentori@users.noreply.github.com>
2021-07-19 16:19:47 +0200

    Delete target directory

--- target/classes/db/migration/v2__create_file_schema.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists files (
-                       id bigint not null auto_increment,
-                       file_name varchar(30),
-                       primary key (id)
-)
